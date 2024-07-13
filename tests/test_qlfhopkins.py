821db2368ae43e149205c068cca42b20c4ac5fde
tomas-cabrera <tcabrera@andrew.cmu.edu>
2024-07-12 23:09:40 -0400

    Added qlfhopkins test

--- tests/test_qlfhopkins.py
+++ tests/test_qlfhopkins.py
@@ -1,1 +1,12 @@
+import pytest
+
 from myagn.qlfhopkins import qlfhopkins
+
+
+def test_number_density():
+    """
+    Check the qlf number density for a few redshifts.
+    """
+    for z, n in {0.05: 1.250986e-02, 0.5: 7.015659e-03, 5: 1.561466e-04}.items():
+        qlf = qlfhopkins.compute(0, z)
+        assert qlf["comoving_number_density"][0] == pytest.approx(n, rel=1e-3)
